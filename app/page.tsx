ee5794714ff27531ec9029457e7fdef1a4ed47fa
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-06-26 14:16:55 +0200

    remove kafka

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,9 @@
-'use client'
-
 import { Button } from '@navikt/ds-react'
-import { useState } from 'react';
 
 export default function Home() {
-  let [success, setSuccess] = useState(false);
-  const kafkaCall = () => {
-    fetch('api/kafka')
-    .then((res) => {
-      setSuccess(res.status === 200);
-    })
-  }
   return (
     <main>
-      {success && <h1>Success!</h1>}
-      <Button onClick={kafkaCall}>
+      <Button>
         trykk her!
       </Button>
     </main>

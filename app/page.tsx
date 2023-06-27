d819950e9a7342f0ee62a9b47f92e46cc5fb2a05
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-06-27 15:01:10 +0200

    add header + experiments

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 'use client'
 
-import { Button } from '@navikt/ds-react'
+import { Button, InternalHeader } from '@navikt/ds-react'
+import { useState } from 'react'
 
 export default function Home() {
+  const [clicked, setClicked] = useState(false);
+  const toggleClicked = () => {
+    setClicked(!clicked);
+  }
   return (
-    <main>
-      <Button>
-        trykk her!
-      </Button>
+    <main className="flex flex-grow">
+      <section className='w-screen flex-grow flex justify-center items-center'>
+        <Button variant={clicked ? "danger" : "primary"} onClick={toggleClicked}>
+          trykk her!
+        </Button>
+      </section>
     </main>
   )
 }

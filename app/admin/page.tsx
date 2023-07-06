4ee890a59a81d74e371d17209522e8cb596e9df9
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-06 14:53:08 +0200

    Nå er vi good :--)
    Co-authored-by: Eline Jørgensen <eline.jorgensen@nav.no>
    Co-authored-by: Kent Daleng <kent.daleng@nav.no>

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,15 @@
+import { getDeltaBackendAccessToken } from "@/auth/token";
 import { backendUrl } from "@/toggles/utils";
 
 export default async function Events() {
+  const accessToken = await getDeltaBackendAccessToken();
+
+  const headers: HeadersInit = {};
+  if (accessToken !== null) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
   const response = await fetch(`${backendUrl()}/admin/event`, {
+    headers,
     next: { revalidate: 0 },
   });
 

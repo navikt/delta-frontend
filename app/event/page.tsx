4c190bfdc1a2aca3ab4348305412e768d4d4ce6b
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-06 10:05:23 +0200

    Add outbound rule for delta-backend, use service discovery
    Co-authored-by: Victor Sebastian I K Bugge <victor.sebastian.i.k.bugge@nav.no>
    Co-authored-by: Eline Jørgensen <eline.jorgensen@nav.no>
    Co-authored-by: Kent Daleng <kent.daleng@nav.no>

--- app/event/page.tsx
+++ app/event/page.tsx
@@ -1,10 +1,12 @@
 import { DeltaEvent } from "@/components/createEvent/createEvent";
 
 export default async function Events() {
-  const response = await fetch("https://delta-backend.intern.dev.nav.no/event", {next: {revalidate: 0}});
+  const response = await fetch("http://delta-backend/event", {
+    next: { revalidate: 0 },
+  });
 
-  const events:(DeltaEvent & {id:number})[] = await response.json();
-  
+  const events: (DeltaEvent & { id: number })[] = await response.json();
+
   return (
     <div>
       {events.map((event) => (
--- components/createEvent/createEvent.ts
+++ components/createEvent/createEvent.ts
@@ -9,20 +9,17 @@ export type DeltaEvent = {
 
 export async function createEvent(formData: FormData) {
   console.log(formData);
-  const response = await fetch(
-    "https://delta-backend.intern.dev.nav.no/event",
-    {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      //TODO:det her er ikke bra nok
-      body: JSON.stringify({
-        title: formData.get("title")?.toString() ?? "",
-        description: formData.get("description")?.toString() ?? "",
-        startTime: formData.get("startTime")?.toString() ?? "",
-        endTime: formData.get("endTime")?.toString() ?? "",
-      } satisfies DeltaEvent),
-    }
-  );
+  const response = await fetch("http://delta-backend/event", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    //TODO:det her er ikke bra nok
+    body: JSON.stringify({
+      title: formData.get("title")?.toString() ?? "",
+      description: formData.get("description")?.toString() ?? "",
+      startTime: formData.get("startTime")?.toString() ?? "",
+      endTime: formData.get("endTime")?.toString() ?? "",
+    } satisfies DeltaEvent),
+  });
 
   console.log(response.status);
 }

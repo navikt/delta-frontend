ce85b0a500acf996e7eb5d6b7191e0205e47fb52
Kent D <kent.daleng@nav.no>
2023-07-07 09:16:20 +0200

    make a dropdown menu :----D

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,16 +1,42 @@
 "use client";
 
 import type { User } from "@/auth/token";
-import { InternalHeader } from "@navikt/ds-react";
+import { MenuGridIcon } from "@navikt/aksel-icons";
+import { Dropdown, InternalHeader } from "@navikt/ds-react";
+import { useRouter } from "next/navigation";
 
 type HeaderProps = { user: User };
 export default function Header({ user }: HeaderProps) {
+  const router = useRouter()
+  
   return (
     <InternalHeader className="flex justify-between flex-grow-0">
       <InternalHeader.Title as="h1" className="whitespace-nowrap">
         Delta Δ
       </InternalHeader.Title>
-      <InternalHeader.User name={`${user.firstName} ${user.lastName}`} />
+      <div className="flex">
+        <Dropdown>
+          <InternalHeader.Button as={Dropdown.Toggle}>
+            <MenuGridIcon className="text-[1.5rem]"
+              title="Arrangementer"
+            />
+          </InternalHeader.Button>
+          <Dropdown.Menu>
+            <Dropdown.Menu.GroupedList>
+              <Dropdown.Menu.GroupedList.Heading>
+                Arrangementer
+              </Dropdown.Menu.GroupedList.Heading>
+              <Dropdown.Menu.GroupedList.Item onClick={() => router.push("/event/new")}>
+                Nytt arrangement
+              </Dropdown.Menu.GroupedList.Item>
+              <Dropdown.Menu.GroupedList.Item>
+                Mine arrangemeneter
+              </Dropdown.Menu.GroupedList.Item>
+            </Dropdown.Menu.GroupedList>
+          </Dropdown.Menu>
+        </Dropdown>
+        <InternalHeader.User name={`${user.firstName} ${user.lastName}`} />
+      </div>
     </InternalHeader>
   );
 }

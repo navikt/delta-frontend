a405a155a03ed66ebacc8822ac08853231d6c47a
Eline Jørgensen <eline.jorgensen@nav.no>
2023-07-10 14:22:56 +0200

    move format out

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -3,47 +3,12 @@
 import { DeltaEvent } from "@/types/event";
 import { BodyLong, Detail, LinkPanel } from "@navikt/ds-react";
 import { CalendarIcon, ClockIcon, PersonIcon } from "@navikt/aksel-icons";
-import { format, formatDuration, intervalToDuration, parseISO } from "date-fns";
-import { getTimezoneOffset } from "date-fns-tz";
-import nb from "date-fns/locale/nb";
 import Link from "next/link";
+import { formatEventDuration, formatEventTimes } from "./format";
 
 type EventCardProps = { event: DeltaEvent };
 
 export function EventCard({ event }: EventCardProps) {
-  const fmt = "do MMMM yyyy, HH:mm";
-
-  const dates = (event: DeltaEvent): [Date, Date] => {
-    const offset = getTimezoneOffset("Europe/Oslo");
-    var start = parseISO(event.startTime);
-    var end = parseISO(event.endTime);
-    start.setTime(start.getTime() - offset);
-    end.setTime(end.getTime() - offset);
-    return [start, end];
-  };
-
-  const isSameDay = (start: Date, end: Date): boolean => {
-    return format(start, "do MM yyyy") === format(end, "do MM yyyy");
-  };
-
-  const formatEventDuration = (event: DeltaEvent): string => {
-    const [start, end] = dates(event);
-
-    return formatDuration(intervalToDuration({ start, end }), {
-      locale: nb,
-      delimiter: ", ",
-    });
-  };
-
-  const formatEventTimes = (event: DeltaEvent): string => {
-    const [start, end] = dates(event);
-
-    return `${format(start, fmt, { locale: nb })} - ${format(
-      end,
-      isSameDay(start, end) ? "HH:mm" : fmt,
-      { locale: nb }
-    )}`;
-  };
 
   return (
     <LinkPanel
---
+++ src/components/format.ts
@@ -0,0 +1,38 @@
+import { DeltaEvent } from "@/types/event";
+import { format, formatDuration, intervalToDuration, parseISO } from "date-fns";
+import { getTimezoneOffset } from "date-fns-tz";
+import nb from "date-fns/locale/nb";
+
+const fmt = "do MMMM yyyy, HH:mm";
+
+export const formatEventTimes = (event: DeltaEvent): string => {
+  const [start, end] = dates(event);
+
+  return `${format(start, fmt, { locale: nb })} - ${format(
+    end,
+    isSameDay(start, end) ? "HH:mm" : fmt,
+    { locale: nb }
+  )}`;
+};
+
+export const isSameDay = (start: Date, end: Date): boolean => {
+  return format(start, "do MM yyyy") === format(end, "do MM yyyy");
+};
+
+export const formatEventDuration = (event: DeltaEvent): string => {
+  const [start, end] = dates(event);
+
+  return formatDuration(intervalToDuration({ start, end }), {
+    locale: nb,
+    delimiter: ", ",
+  });
+};
+
+export const dates = (event: DeltaEvent): [Date, Date] => {
+  const offset = getTimezoneOffset("Europe/Oslo");
+  var start = parseISO(event.startTime);
+  var end = parseISO(event.endTime);
+  start.setTime(start.getTime() - offset);
+  end.setTime(end.getTime() - offset);
+  return [start, end];
+};

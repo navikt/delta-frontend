11ac14002893ea852f1920497a81a394d87489b3
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-10 10:37:47 +0200

    Add key for each eventCard in eventList

--- src/components/eventList.tsx
+++ src/components/eventList.tsx
@@ -8,7 +8,7 @@ export default function EventList({ events }: EventListProps) {
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
       {events.map((event) => (
-        <EventCard event={event} />
+        <EventCard event={event} key={`event-${event.id}`} />
       ))}
     </section>
   );

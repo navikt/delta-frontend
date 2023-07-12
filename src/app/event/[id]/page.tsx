0c1bb4a56d07f55083c510f5fec687fa142110ef
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-12 16:56:37 +0200

    Change event setup by adding participants and user to the props so that it is possible to identify if current user is signed up.

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -2,10 +2,12 @@ import type { DeltaEventWithParticipant } from "@/types/event";
 import { notFound } from "next/navigation";
 import { Event } from "@/components/event";
 import { getAuthlessApi } from "@/api/instance";
+import { getUser } from "@/auth/token";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const api = getAuthlessApi();
   const response = await api.get(`/event/${params.id}`);
+  const user = getUser();
 
   // Feilhåndtering
   if (response.status === 404) {
@@ -29,7 +31,7 @@ export default async function Page({ params }: { params: { id: string } }) {
   return (
     <main className="flex flex-grow">
       <section className="w-screen flex-grow flex justify-center items-center flex-col gap-3">
-        <Event event={event}></Event>
+        <Event event={event} participants={participants} user={user}></Event>
       </section>
     </main>
   );

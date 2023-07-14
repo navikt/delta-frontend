bd1dbb1cdde80b9d19fd739ad0323a396325b7bc
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-14 10:38:03 +0200

    Format date and time to iso in create event page.
    Co-authored-by: Victor Sebastian I K Bugge <victor.sebastian.i.k.bugge@nav.no>

--- src/app/event/new/createEvent.ts
+++ src/app/event/new/createEvent.ts
@@ -5,16 +5,34 @@ import { redirect } from "next/navigation";
 
 export async function createEvent(formData: FormData) {
   const api = await getAuthApi();
+
+  console.log(formData.get("startTime"));
   const response = await api.put("/admin/event", {
     title: getFormDataString(formData, "title"),
     description: getFormDataString(formData, "description"),
     location: getFormDataString(formData, "location"),
-    startTime: getFormDataString(formData, "startTime"),
-    endTime: getFormDataString(formData, "endTime"),
+    startTime: `${formatDate(
+      getFormDataString(formData, "startDate")
+    )}T${getFormDataString(formData, "startTime")}:00`,
+    endTime: `${formatDate(
+      getFormDataString(formData, "endDate")
+    )}T${getFormDataString(formData, "endTime")}:00`,
   });
   redirect(`/event/${response.data.id}`);
 }
 
 function getFormDataString(formData: FormData, key: string): string {
   return formData.get(key)?.toString() ?? "";
+}
+
+/**
+ * Converts a date on the format dd.mm.yyyy to yyyy-mm-dd
+ * @param date string on the format dd.mm.yyyy
+ * @returns string on the format yyyy-mm-dd
+ */
+function formatDate(date: string): string {
+  const [day, month, year] = date.split(".").map((s) => parseInt(s));
+  return `${year.toString().padStart(4, "0")}-${month
+    .toString()
+    .padStart(2, "0")}-${day.toString().padStart(2, "0")}`;
 }

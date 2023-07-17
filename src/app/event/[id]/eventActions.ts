73d8475c0ee6fb41759cca7428893ce666249c9a
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-17 17:25:25 +0200

    move user auth endpoints to /user

--- src/app/event/[id]/eventActions.ts
+++ src/app/event/[id]/eventActions.ts
@@ -4,10 +4,10 @@ import { getAuthApi } from "@/api/instance";
 
 export async function joinEvent(formData: FormData) {
   const api = await getAuthApi();
-  const response = await api.post(`/event/${formData.get("id")}`);
+  const response = await api.post(`/user/event/${formData.get("id")}`);
 }
 
 export async function leaveEvent(formData: FormData) {
   const api = await getAuthApi();
-  const response = await api.delete(`/event/${formData.get("id")}`);
+  const response = await api.delete(`/user/event/${formData.get("id")}`);
 }

9c70e69de68000fdf864007270fd56e1a5c254d5
Kent D <kent.daleng@nav.no>
2023-07-18 13:48:41 +0200

    make new event form look more like an event page

--- src/app/event/new/page.tsx
+++ src/app/event/new/page.tsx
@@ -6,17 +6,19 @@ export default async function NewEvent() {
   await checkToken("/event/new");
 
   return (
-    <div className="max-w-[95%] w-[80rem] gap-7 flex flex-col">
-      <div className="flex flex-col gap-2">
-        <Heading level="1" size="large">
+    <div className="w-full flex flex-col align-center items-center">
+      <div className="w-full text-center h-fit bg-green-200 p-18 pb-24">
+        <Heading level="1" size="xlarge">
           Opprett arrangement
         </Heading>
+      </div>
+      <div className="bg-white drop-shadow-lg border-gray-200 border-2 rounded relative w-5/6 top-[-5rem] z-10 flex flex-col p-4 h-fit max-w-[80rem] gap-4">
         <p className="italic break-words">
           Arrangementet vil være synlig for alle som har tilgang til Delta, og
           vil bli publisert på deltakalenderen.
         </p>
+        <CreateEventForm />
       </div>
-      <CreateEventForm />
     </div>
   );
 }

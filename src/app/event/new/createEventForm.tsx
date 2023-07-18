cda01107e7112102f5da3a900c9edb7f96da0b01
Kent D <kent.daleng@nav.no>
2023-07-18 13:18:25 +0200

    zod + react-form-hooks for new event page

--- src/app/event/new/createEventForm.tsx
+++ src/app/event/new/createEventForm.tsx
@@ -1,66 +1,118 @@
 "use client";
 
-import {
-  Button,
-  DatePicker,
-  TextField,
-  Textarea,
-  useRangeDatepicker,
-} from "@navikt/ds-react";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Button, TextField, Textarea } from "@navikt/ds-react";
+import { useState } from "react";
 import { createEvent } from "./createEvent";
 import { redirect } from "next/navigation";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import EventDatepicker from "./eventDatepicker";
+
+const createEventSchema = z
+  .object({
+    title: z.string().nonempty({ message: "Du må fylle inn en tittel" }),
+    location: z.string().nonempty({ message: "Du må fylle inn et sted" }),
+    description: z
+      .string()
+      .nonempty({ message: "Du må fylle inn en beskrivelse" }),
+    startDate: z.date({ required_error: "Du må velge en startdato" }),
+    startTime: z.string().regex(/[0-9]{2}:[0-9]{2}/, {
+      message: "Verdien må være et gyldig tidspunkt",
+    }),
+    endDate: z.date(),
+    endTime: z
+      .string({ required_error: "Du må velge en sluttid" })
+      .regex(/[0-9]{2}:[0-9]{2}/, {
+        message: "Verdien må være et gyldig tidspunkt",
+      }),
+  })
+  .required();
+
+export type CreateEventSchema = z.infer<typeof createEventSchema>;
 
 export default function CreateEventForm() {
-  const { datepickerProps, toInputProps, fromInputProps } = useRangeDatepicker({
-    fromDate: new Date("Aug 23 2019"),
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    trigger,
+    getValues,
+    control,
+    formState: { errors },
+  } = useForm<CreateEventSchema>({
+    resolver: zodResolver(createEventSchema),
   });
 
-  const [loading, setLoading] = useState(false);
   return (
     <form
-      action={(f) => createAndRedirect(f, setLoading)}
+      action={async () => {
+        const valid = await trigger();
+        setLoading(true);
+        if (valid) createAndRedirect(getValues());
+        setLoading(false);
+      }}
       className="flex flex-col gap-5"
     >
-      <TextField label="Tittel" name="title" className="" required />
-      <TextField label="Sted" name="location" required />
-      <Textarea label="Beskrivelse" name="description" required />
-      <DatePicker {...datepickerProps} style={{ width: "100%" }}>
-        <div className="flex flex-row flex-wrap justify-left gap-4 pb-0 items-end">
-          <div className="flex flex-col sm:flex-row items-start sm:items-end gap-4 flex-wrap ">
-            <DatePicker.Input
-              {...fromInputProps}
-              label="Fra"
-              name="startDate"
-              required
-            />
-            <div className="navds-form-field navds-form-field--medium">
-              <input
-                type="time"
-                name="startTime"
-                className="navds-text-field__input"
-                required
-              />
-            </div>
-          </div>
-          <div className="flex flex-col sm:flex-row items-start sm:items-end gap-4 flex-wrap ">
-            <DatePicker.Input
-              {...toInputProps}
-              label="Til"
-              name="endDate"
-              required
-            />
-            <div className="navds-form-field navds-form-field--medium">
-              <input
-                type="time"
-                name="endTime"
-                className="navds-text-field__input"
-                required
-              />
-            </div>
-          </div>
+      <TextField
+        label="Tittel"
+        {...register("title")}
+        error={errors.title?.message}
+      />
+      <TextField
+        label="Sted"
+        {...register("location")}
+        error={errors.location?.message}
+      />
+      <Textarea
+        label="Beskrivelse"
+        {...register("description")}
+        error={errors.description?.message}
+      />
+      <div className="flex flex-row flex-wrap justify-left gap-4 pb-0 items-end">
+        <EventDatepicker
+          name="startDate"
+          label="Fra"
+          invalidMessage="Du må fylle inn en gyldig startdato"
+          requiredMessage="Du må fylle inn en startdato"
+          control={control}
+          errors={errors}
+        />
+        <div className="navds-form-field navds-form-field--medium">
+          <input
+            type="time"
+            className="navds-text-field__input w-20"
+            {...register("startTime")}
+          />
+          {errors.startTime && (
+            <p className="navds-error-message navds-label">
+              {errors.startTime.message}
+            </p>
+          )}
         </div>
-      </DatePicker>
+      </div>
+      <div className="flex flex-row flex-wrap justify-left gap-4 pb-0 items-end">
+        <EventDatepicker
+          name="endDate"
+          label="Til"
+          invalidMessage="Du må fylle inn en gyldig sluttdato"
+          requiredMessage="Du må fylle inn en sluttdato"
+          control={control}
+          errors={errors}
+        />
+        <div className="navds-form-field navds-form-field--medium">
+          <input
+            type="time"
+            className="navds-text-field__input w-20"
+            {...register("endTime")}
+          />
+          {errors.endTime && (
+            <p className="navds-error-message navds-label">
+              {errors.endTime.message}
+            </p>
+          )}
+        </div>
+      </div>
       <Button type="submit" className="w-[19rem] max-w-full" loading={loading}>
         Opprett arrangement
       </Button>
@@ -68,11 +120,7 @@ export default function CreateEventForm() {
   );
 }
 
-async function createAndRedirect(
-  formData: FormData,
-  setIsLoading: Dispatch<SetStateAction<boolean>>,
-) {
-  setIsLoading(true);
+async function createAndRedirect(formData: CreateEventSchema) {
   const event = await createEvent(formData);
   redirect(`/event/${event.id}`);
 }

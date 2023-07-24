b33611395391c4c327a4e778ae95253c42eb9c18
Eline Jørgensen <eline.jorgensen@nav.no>
2023-07-24 16:12:23 +0200

    fix  edit event button

--- src/app/event/[id]/eventDetails.tsx
+++ src/app/event/[id]/eventDetails.tsx
@@ -81,7 +81,7 @@ export default function EventDetails({
             )}
           </div>
           {(function () {
-            if (false) {
+            if (event.ownerEmail === user.email) {
               return (
                 <>
                   <Link

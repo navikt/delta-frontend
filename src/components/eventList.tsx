f66b3f9e16912c6aeabf4e77f0cf441e7abbe401
Sebastian Bugge <victor.sebastian.i.k.bugge@nav.no>
2023-07-25 10:53:38 +0200

    add skeleton loading

--- src/components/eventList.tsx
+++ src/components/eventList.tsx
@@ -2,14 +2,24 @@
 
 import { DeltaEvent } from "@/types/event";
 import { EventCard } from "@/components/eventCard";
+import { Skeleton } from "@navikt/ds-react";
 
-type EventListProps = { events: DeltaEvent[] };
-export default function EventList({ events }: EventListProps) {
+type EventListProps = { events: DeltaEvent[]; loading: boolean };
+export default function EventList({ events, loading }: EventListProps) {
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {events.map((event) => (
-        <EventCard event={event} key={`event-${event.id}`} />
-      ))}
+      {loading ? (
+        <>
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+        </>
+      ) : (
+        events.map((event) => (
+          <EventCard event={event} key={`event-${event.id}`} />
+        ))
+      )}
     </section>
   );
 }

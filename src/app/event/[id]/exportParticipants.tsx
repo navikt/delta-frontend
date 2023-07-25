437efea786e91abf7eab0da605ece229980d61e5
Eline Jørgensen <eline.jorgensen@nav.no>
2023-07-25 11:11:52 +0200

    new header design
    Co-authored-by: Victor Sebastian I K Bugge <victor.sebastian.i.k.bugge@nav.no>

--- src/app/event/[id]/exportParticipants.tsx
+++ src/app/event/[id]/exportParticipants.tsx
@@ -6,28 +6,26 @@ import { Button, CopyButton, Dropdown, Link } from "@navikt/ds-react";
 type ExportParticipantsProps = { participants: DeltaParticipant[] };
 
 export default function ExportParticipants(
-  participants: ExportParticipantsProps
+  participants: ExportParticipantsProps,
 ) {
   const copyEmails = participants.participants.map((p) => p.email).join(";");
   const sendEmails = participants.participants.map((p) => p.email).join(", ");
 
   return (
-    <div className="min-h-32">
-      <Dropdown>
-        <Button as={Dropdown.Toggle} variant="secondary">
-          <span className="flex flex-row gap-1 whitespace-nowrap">
-            Eksporter deltakere <ChevronDownIcon />
-          </span>
-        </Button>
-        <Dropdown.Menu>
-          <Dropdown.Menu.List>
-            <CopyButton copyText={copyEmails} text="Kopier alle eposter" />
-            <Dropdown.Menu.List.Item as={Link} href={`mailto:${sendEmails}`}>
-              <EnvelopeClosedIcon /> Send e-post til deltakere
-            </Dropdown.Menu.List.Item>
-          </Dropdown.Menu.List>
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
+    <Dropdown>
+      <Button as={Dropdown.Toggle} variant="secondary" className="w-full">
+        <span className="flex flex-row gap-1 whitespace-nowrap items-center">
+          Eksporter <ChevronDownIcon className="h-6 w-6" />
+        </span>
+      </Button>
+      <Dropdown.Menu>
+        <Dropdown.Menu.List>
+          <CopyButton copyText={copyEmails} text="Kopier alle eposter" />
+          <Dropdown.Menu.List.Item as={Link} href={`mailto:${sendEmails}`}>
+            <EnvelopeClosedIcon /> Send e-post til deltakere
+          </Dropdown.Menu.List.Item>
+        </Dropdown.Menu.List>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }

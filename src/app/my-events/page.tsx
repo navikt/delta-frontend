1180aa577093d4bdabf53f8453fe759d1d35c01f
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-26 14:56:56 +0200

    change var to const in my-event

--- src/app/my-events/page.tsx
+++ src/app/my-events/page.tsx
@@ -1,10 +1,9 @@
 import CardWithBackground from "@/components/cardWithBackground";
 import EventList from "@/components/eventList";
 import { getEvents } from "@/service/eventActions";
-import { DeltaEvent } from "@/types/event";
 
 export default async function MyEvents() {
-  var events: DeltaEvent[] = await getEvents({ onlyMine: true });
+  const events = await getEvents({ onlyMine: true });
 
   return (
     <CardWithBackground color="bg-blue-200" title="Mine arrangementer">

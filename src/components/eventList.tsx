f2661058eaeca15fa8d9ffedc2d440a127a3a1f3
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-28 12:22:34 +0200

    Represent EventList as a list, to make eventlist more accessible for screen readers

--- src/components/eventList.tsx
+++ src/components/eventList.tsx
@@ -8,22 +8,26 @@ type EventListProps = { events: DeltaEvent[]; loading: boolean };
 export default function EventList({ events, loading }: EventListProps) {
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      {loading ? (
-        <>
-          <Skeleton variant="rounded" />
-          <Skeleton variant="rounded" />
-          <Skeleton variant="rounded" />
-          <Skeleton variant="rounded" />
-        </>
-      ) : events.length ? (
-        events.map((event) => (
-          <EventCard event={event} key={`event-${event.id}`} />
-        ))
-      ) : (
-        <p className="text-center col-span-full italic text-xlarge">
-          Fant ingen arrangementer :--(
-        </p>
-      )}
+      <ul>
+        {loading ? (
+          <>
+            <Skeleton variant="rounded" />
+            <Skeleton variant="rounded" />
+            <Skeleton variant="rounded" />
+            <Skeleton variant="rounded" />
+          </>
+        ) : events.length ? (
+          events.map((event) => (
+            <li>
+              <EventCard event={event} key={`event-${event.id}`} />
+            </li>
+          ))
+        ) : (
+          <p className="text-center col-span-full italic text-xlarge">
+            Fant ingen arrangementer :--(
+          </p>
+        )}
+      </ul>
     </section>
   );
 }

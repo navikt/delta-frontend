c986bbf1a2621f86f6e293731632f68fad55a483
Nima Hakimi <nima.hakimi2@nav.no>
2023-07-31 09:56:48 +0200

    Remove section tag in eventlist

--- src/components/eventList.tsx
+++ src/components/eventList.tsx
@@ -7,27 +7,25 @@ import { Skeleton } from "@navikt/ds-react";
 type EventListProps = { events: DeltaEvent[]; loading: boolean };
 export default function EventList({ events, loading }: EventListProps) {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-3 gap-4">
-      <ul>
-        {loading ? (
-          <>
-            <Skeleton variant="rounded" />
-            <Skeleton variant="rounded" />
-            <Skeleton variant="rounded" />
-            <Skeleton variant="rounded" />
-          </>
-        ) : events.length ? (
-          events.map((event) => (
-            <li key={`event-${event.id}`}>
-              <EventCard event={event} />
-            </li>
-          ))
-        ) : (
-          <p className="text-center col-span-full italic text-xlarge">
-            Fant ingen arrangementer :--(
-          </p>
-        )}
-      </ul>
-    </section>
+    <ul className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {loading ? (
+        <>
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+          <Skeleton variant="rounded" />
+        </>
+      ) : events.length ? (
+        events.map((event) => (
+          <li key={`event-${event.id}`}>
+            <EventCard event={event} />
+          </li>
+        ))
+      ) : (
+        <p className="text-center col-span-full italic text-xlarge">
+          Fant ingen arrangementer :--(
+        </p>
+      )}
+    </ul>
   );
 }

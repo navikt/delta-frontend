d6aa738b9431454c2e90cff70af101ff4a867d0d
Eline Jørgensen <eline.jorgensen@nav.no>
2023-08-04 13:08:38 +0200

    Put header elements inside hamburger menu in mobile view
    Co-authored-by: Victor Sebastian I K Bugge <victor.sebastian.i.k.bugge@nav.no>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,11 +9,13 @@ import {
   PlusIcon,
 } from "@navikt/aksel-icons";
 import { Button, Dropdown } from "@navikt/ds-react";
+import { useMediaQuery } from "react-responsive";
 import Link from "next/link";
 
 export default function Header() {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const linkButton =
-    "flex items-center no-underline text-text-subtle bg-transparent hover:bg-border-subtle-hover navds-button navds-button--primary navds-button--medium";
+    "flex no-underline items-center text-text-subtle bg-transparent hover:bg-border-subtle-hover navds-button navds-button--primary navds-button--medium";
   return (
     <header className="flex pt-3 z-10 items-center w-full max-w-[80rem] m-auto">
       <div className="flex items-stretch">
@@ -23,24 +25,51 @@ export default function Header() {
         <div className="w-1 border-r-[1px] border-border-subtle border-solid"></div>
         <div className="w-1 border-l-[1px] border-border-subtle border-solid"></div>
       </div>
-      <div className="flex items-center justify-between w-full">
-        <div className="flex items-center w-full">
+      {isMobile ? (
+        <Dropdown>
+          <Button as={Dropdown.Toggle} className={linkButton}>
+            <MenuHamburgerIcon fontSize="1.5rem" />
+          </Button>
+          <Dropdown.Menu className="w-auto">
+            <Dropdown.Menu.List>
+              <Dropdown.Menu.List.Item as={Link} href="/joined-events">
+                <CheckmarkCircleIcon fontSize="1.5rem" />
+                <span>Påmeldte</span>
+              </Dropdown.Menu.List.Item>
+              <Dropdown.Menu.List.Item as={Link} href="/my-events">
+                <PencilIcon fontSize="1.5rem" />
+                <span className="whitespace-nowrap">Arrangert av meg</span>
+              </Dropdown.Menu.List.Item>
+              <Dropdown.Menu.List.Item as={Link} href="/event/new">
+                <PlusIcon fontSize="1.5rem" />
+                <span className="whitespace-nowrap">
+                  Opprett nytt arrangement
+                </span>
+              </Dropdown.Menu.List.Item>
+            </Dropdown.Menu.List>
+          </Dropdown.Menu>
+        </Dropdown>
+      ) : (
+        <div className="flex items-center justify-between w-full">
           <Link href="/joined-events" className={linkButton}>
             <CheckmarkCircleIcon fontSize="1.5rem" />
             Påmeldte
           </Link>
+          <div className="flex items-center w-full"></div>
+          <div className="flex flex-grow">
+            <Link href="/my-events" className={linkButton}>
+              <PencilIcon fontSize="1.5rem" />
+              <span className="whitespace-nowrap">Arrangert av meg</span>
+            </Link>
+            <Link className={linkButton} href="/event/new">
+              <PlusIcon fontSize="1.5rem" />
+              <span className="whitespace-nowrap">
+                Opprett nytt arrangement
+              </span>
+            </Link>
+          </div>
         </div>
-        <div className="flex flex-grow">
-          <Link href="/my-events" className={linkButton}>
-            <PencilIcon fontSize="1.5rem" />
-            <span className="whitespace-nowrap">Arrangert av meg</span>
-          </Link>
-          <Link className={linkButton} href="/event/new">
-            <PlusIcon fontSize="1.5rem" />
-            <span className="whitespace-nowrap">Opprett nytt arrangement</span>
-          </Link>
-        </div>
-      </div>
+      )}
     </header>
   );
 }

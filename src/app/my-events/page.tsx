bb2e1739a44c058ec5de4a1b3ef0a2910043c8c8
Eline Jørgensen <eline.jorgensen@nav.no>
2023-08-04 09:49:21 +0200

    remove unused imports

--- src/app/my-events/page.tsx
+++ src/app/my-events/page.tsx
@@ -1,5 +1,5 @@
 import CardWithBackground from "@/components/cardWithBackground";
-import EventList from "@/components/eventList";
+import EventFilters from "@/components/eventFilters";
 import { getEvents } from "@/service/eventActions";
 
 export default async function MyEvents() {

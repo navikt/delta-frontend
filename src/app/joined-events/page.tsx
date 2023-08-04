b237f8508570ac1a960bdcc3871bdccb235d165f
Eline Jørgensen <eline.jorgensen@nav.no>
2023-08-04 09:57:24 +0200

    add joined events page

---
+++ src/app/joined-events/page.tsx
@@ -0,0 +1,17 @@
+import CardWithBackground from "@/components/cardWithBackground";
+import EventFilters from "@/components/eventFilters";
+import { getEvents } from "@/service/eventActions";
+
+export default async function MyEvents() {
+  const events = await getEvents({ onlyMine: true });
+
+  return (
+    <CardWithBackground
+      color="bg-blue-200"
+      title="Påmeldte arrangementer"
+      backLink="/"
+    >
+      <EventFilters onlyJoined selectTime />
+    </CardWithBackground>
+  );
+}

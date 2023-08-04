488a2f6300bf9f0fc12519fed56cb2dd7faaec0d
Eline Jørgensen <eline.jorgensen@nav.no>
2023-08-04 11:13:46 +0200

    move everything out of dropdown
    Co-authored-by: Victor Sebastian I K Bugge <victor.sebastian.i.k.bugge@nav.no>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,56 +12,34 @@ import { Button, Dropdown } from "@navikt/ds-react";
 import Link from "next/link";
 
 export default function Header() {
+  const linkButton =
+    "flex items-center no-underline text-text-subtle bg-transparent hover:bg-border-subtle-hover navds-button navds-button--primary navds-button--medium";
   return (
-    <header className="flex flex-row pt-3 z-10 gap-6 items-center justify-between w-full md:px-6 max-w-[80rem] m-auto">
-      <Link
-        className="flex items-center no-underline text-text-subtle"
-        href="/"
-      >
-        <span className="w-fit flex gap-2 items-center flex-row whitespace-nowrap text-3xl text-text-subtle bg-transparent hover:bg-border-subtle-hover navds-button navds-button--primary navds-button--medium">
-          Δ Delta
-        </span>
-      </Link>
-      <div className="flex">
-        <Link
-          className="flex items-center no-underline text-text-subtle "
-          href="/event/new"
-        >
-          <span className="flex items-center text-text-subtle bg-transparent hover:bg-border-subtle-hover navds-button navds-button--primary navds-button--medium">
-            <PlusIcon fontSize="1.5rem" /> Opprett nytt arrangement
-          </span>
+    <header className="flex pt-3 z-10 items-center w-full max-w-[80rem] m-auto">
+      <div className="flex items-stretch">
+        <Link className={linkButton} href="/">
+          <span className="text-2xl whitespace-nowrap">Δ Delta</span>
         </Link>
-        <Dropdown>
-          <Button
-            as={Dropdown.Toggle}
-            className="bg-transparent text-text-subtle hover:bg-border-subtle-hover"
-          >
-            <div className="flex items-center gap-2 font-normal">
-              <>Mine arrangementer</>
-              <ChevronDownIcon />
-            </div>
-          </Button>
-          <Dropdown.Menu>
-            <Dropdown.Menu.GroupedList>
-              <Dropdown.Menu.GroupedList.Item
-                as={Link}
-                href="/joined-events"
-                className="no-underline"
-              >
-                <CheckmarkCircleIcon />
-                Påmeldte
-              </Dropdown.Menu.GroupedList.Item>
-              <Dropdown.Menu.GroupedList.Item
-                as={Link}
-                href="/my-events"
-                className="no-underline"
-              >
-                <PencilIcon />
-                Arrangert av meg
-              </Dropdown.Menu.GroupedList.Item>
-            </Dropdown.Menu.GroupedList>
-          </Dropdown.Menu>
-        </Dropdown>
+        <div className="w-1 border-r-[1px] border-border-subtle border-solid"></div>
+        <div className="w-1 border-l-[1px] border-border-subtle border-solid"></div>
+      </div>
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center w-full">
+          <Link href="/joined-events" className={linkButton}>
+            <CheckmarkCircleIcon fontSize="1.5rem" />
+            Påmeldte
+          </Link>
+        </div>
+        <div className="flex flex-grow">
+          <Link href="/my-events" className={linkButton}>
+            <PencilIcon fontSize="1.5rem" />
+            <span className="whitespace-nowrap">Arrangert av meg</span>
+          </Link>
+          <Link className={linkButton} href="/event/new">
+            <PlusIcon fontSize="1.5rem" />
+            <span className="whitespace-nowrap">Opprett nytt arrangement</span>
+          </Link>
+        </div>
       </div>
     </header>
   );

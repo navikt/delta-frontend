8a79e7ccd73142fd792eefc567b933a84111303e
Eline Jørgensen <eline.jorgensen@nav.no>
2023-08-09 12:44:01 +0200

    fix squashed participant icon

--- src/app/event/[id]/participant.tsx
+++ src/app/event/[id]/participant.tsx
@@ -7,17 +7,17 @@ type ParticipantProps = DeltaParticipant & { owner: boolean };
 export default function Participant(participant: ParticipantProps) {
   return (
     <div className="flex flex-row items-center justify-between w-full gap-2">
-      <div className="flex flex-row items-center gap-2">
-        <ParticipantIcon name={participant.name} type="participantList" />
-        <span className="text-md">
-          {participant.name.split(", ").reverse().join(" ")}
+      <span className="flex items-center gap-2">
+        <span className="flex-none">
+          <ParticipantIcon name={participant.name} type="participantList" />
         </span>
-        {participant.owner && (
-          <span className="bg-gray-500 text-surface-subtle cursor-default rounded-xl px-2">
-            ARRANGØR
-          </span>
-        )}
-      </div>
+        {participant.name.split(", ").reverse().join(" ")}
+      </span>
+      {participant.owner && (
+        <span className="bg-gray-500 text-surface-subtle cursor-default rounded-xl px-2">
+          ARRANGØR
+        </span>
+      )}
     </div>
   );
 }

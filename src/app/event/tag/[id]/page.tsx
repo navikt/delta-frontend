ef852668e44e64856c69453b72359e093e653a53
EIlif Johansen <eilifjohansen@outlook.com>
2023-08-19 12:38:21 +0200

    Testing alternative location for category page

---
+++ src/app/event/tag/[id]/page.tsx
@@ -0,0 +1,39 @@
+import {checkToken} from "@/auth/token";
+import CardWithBackground from "@/components/cardWithBackground";
+import EventFilters from "@/components/eventFiltersCategory";
+import {getAllCategories} from "@/service/eventActions";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: "Kategori Δ Delta",
+};
+
+export default async function Page({
+    params,
+    }: {
+    params: { id: string };
+}) {
+    checkToken(`/event/tag/${params.id}`);
+    const allCategories = await getAllCategories();
+    const category = params.id
+    const theCatogery = allCategories.find(item => item.name === category);
+    const title = category + " arrangementer"
+
+    return (
+        <>
+            <main className="flex flex-grow">
+                {theCatogery ? (
+                    <CardWithBackground
+                        color="bg-blue-200"
+                        title={title}
+                        newEvent
+                    >
+                        <EventFilters categories={[{id: theCatogery.id, name: theCatogery.name}]} searchName/>
+                    </CardWithBackground>
+                ) : (<section className="w-screen flex-grow flex justify-center items-center">
+                    404 - Kategorien finnes ikke
+                </section>)}
+            </main>
+        </>
+    );
+}

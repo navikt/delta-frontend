d89e430ed04038a820eb12bb97d96a6b44e0f8e1
EIlif Johansen <eilifjohansen@outlook.com>
2023-08-19 02:02:54 +0200

    Added category page logic

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -1,25 +1,32 @@
-import { checkToken } from "@/auth/token";
+import {checkToken} from "@/auth/token";
 import CardWithBackground from "@/components/cardWithBackground";
-import EventFilters from "@/components/eventFilters";
-import { getAllCategories } from "@/service/eventActions";
+import EventFilters from "@/components/eventFiltersCategory";
+import {getAllCategories} from "@/service/eventActions";
 
 type EventPageProps = { params: { id: string } };
 
-export default async function Page({ params }: EventPageProps) {
-  await checkToken("/");
-  const allCategories = await getAllCategories();
-  const category = params.id
-  const title = category + " arrangementer"
+export default async function Page({params}: EventPageProps) {
+    await checkToken("/");
+    const allCategories = await getAllCategories();
+    const category = params.id
+    const theCatogery = allCategories.find(item => item.name === category);
+    const title = category + " arrangementer"
 
-  return (
-      <main className="flex flex-grow">
-        <CardWithBackground
-            color="bg-blue-200"
-            title={title}
-            newEvent
-        >
-          <EventFilters categories={allCategories} selectCategory searchName />
-        </CardWithBackground>
-      </main>
-  );
+    return (
+        <>
+            <main className="flex flex-grow">
+                {theCatogery ? (
+                    <CardWithBackground
+                        color="bg-blue-200"
+                        title={title}
+                        newEvent
+                    >
+                        <EventFilters categories={[{id: theCatogery.id, name: theCatogery.name}]} searchName/>
+                    </CardWithBackground>
+                ) : (<section className="w-screen flex-grow flex justify-center items-center">
+                    404 - Kategorien finnes ikke
+                </section>)}
+            </main>
+        </>
+    );
 }

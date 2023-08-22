7b063a64817cdf4e13805dcdda2873bdad7d5330
Nima Hakimi <nima.hakimi2@nav.no>
2023-08-22 18:16:06 +0200

    Add 'Fullt' indicator in eventCard

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import "./eventCard.css";
-import { Category, DeltaEvent } from "@/types/event";
+import { Category, FullDeltaEvent } from "@/types/event";
 import { Detail, Heading, LinkPanel, Tag } from "@navikt/ds-react";
 import {
   CalendarIcon,
@@ -20,34 +20,35 @@ export function EventCard({
   event,
   categories,
 }: {
-  event: DeltaEvent;
+  event: FullDeltaEvent;
   categories: Category[];
 }) {
   const isUtløpt =
-    !!event.signupDeadline && new Date(event.signupDeadline) < new Date()
+    !!event.event.signupDeadline &&
+    new Date(event.event.signupDeadline) < new Date()
       ? true
       : false;
   return (
     <Link
-      href={`/event/${event.id}`}
-      key={`event-${event.id}`}
+      href={`/event/${event.event.id}`}
+      key={`event-${event.event.id}`}
       className="flex flex-col h-full p-4 border rounded-xl text-text-default border-gray-300 transition-all hover:-translate-y-1 hover:scale-105 hover:text-surface-action-selected-hover hover:border-border-action event-card"
     >
-      <Heading size="small">{event.title}</Heading>
+      <Heading size="small">{event.event.title}</Heading>
       <div className="flex flex-col gap-2 h-full justify-between">
         <div>
           <Detail className="flex gap-1 items-center">
             <CalendarIcon title="kalender" />
-            {formatEventTimes(event)}
+            {formatEventTimes(event.event)}
           </Detail>
-          {formatEventDuration(event) !== "" && (
+          {formatEventDuration(event.event) !== "" && (
             <Detail className="flex gap-1 items-center">
               <ClockIcon title="klokke" />
-              {formatEventDuration(event)}
+              {formatEventDuration(event.event)}
             </Detail>
           )}
           <Detail>
-            {event.signupDeadline && (
+            {event.event.signupDeadline && (
               <span className="flex items-center gap-1">
                 <HourglassBottomFilledIcon title="timeglass" />
                 Påmeldingsfrist:{" "}
@@ -56,21 +57,27 @@ export function EventCard({
                     Utløpt
                   </span>
                 ) : (
-                  formatDeadline(event)
+                  formatDeadline(event.event)
                 )}
               </span>
             )}
           </Detail>
           <Detail>
-            {event.participantLimit > 0 && (
-                <>
-                  <span className="flex items-center gap-1">
-                    <PersonCheckmarkIcon title="person" />
-                    Antallsbegrensning: {event.participantLimit} personer
-                  </span>
-                </>
-            )
-            }
+            {event.event.participantLimit > 0 && (
+              <>
+                <span className="flex items-center gap-1">
+                  <PersonCheckmarkIcon title="person" />
+                  {event.participants.length + event.hosts.length}/
+                  {event.event.participantLimit}
+                  {event.participants.length + event.hosts.length >=
+                    event.event.participantLimit && (
+                    <span className="bg-red-600 text-white rounded px-2">
+                      Fullt
+                    </span>
+                  )}
+                </span>
+              </>
+            )}
           </Detail>
         </div>
         <div className="flex gap-2 flex-wrap h-full items-end w-full">

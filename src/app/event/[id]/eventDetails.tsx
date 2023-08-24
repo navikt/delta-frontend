fad241767247fed0ab02ae8259f6308f2b22abf5
EIlif Johansen <eilifjohansen@outlook.com>
2023-08-24 12:47:54 +0200

    Testing description on event page without italic

--- src/app/event/[id]/eventDetails.tsx
+++ src/app/event/[id]/eventDetails.tsx
@@ -199,7 +199,7 @@ eller antallsbegrensing er nådd, kan du ikke melde deg på igjen.`
           <Heading size="medium" as="h2">
             Detaljer
           </Heading>
-          <BodyLong className="italic whitespace-pre-line break-words">
+          <BodyLong className="whitespace-pre-line break-words">
             {event.description}
           </BodyLong>
           <div className="flex gap-2 flex-wrap mt-6 mb-6">

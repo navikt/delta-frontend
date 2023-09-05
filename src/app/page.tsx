a9f4aa35b6583d0b4ca13f3c9178a826ec60157e
EIlif Johansen <eilifjohansen@outlook.com>
2023-09-06 00:17:11 +0200

    Edited meta title on frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,11 @@ import { checkToken } from "@/auth/token";
 import CardWithBackground from "@/components/cardWithBackground";
 import EventFilters from "@/components/eventFilters";
 import { getAllCategories } from "@/service/eventActions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Delta Δ NAV",
+};
 
 export default async function Home(context: any) {
   await checkToken("/");

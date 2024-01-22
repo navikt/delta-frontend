af3f3e4bbe8cdf851bd9f199685fb65a03d5e89a
EIlif Johansen <eilifjohansen@outlook.com>
2024-01-22 23:17:25 +0100

    Endret til blå header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="no" className="min-h-screen">
       <body>
-        <div id="main" className="flex flex-col min-h-screen">
+        <div id="main" className="flex flex-col min-h-screen" style={{background : "#073f64"}}>
           <Header />
           <main className="bg-surface-subtle flex-grow flex justify-center pb-10">
             {children}

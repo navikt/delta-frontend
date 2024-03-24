fa6e33ab78d2e1a1415006169d0c95a9faf02c93
EIlif Johansen <eilifjohansen@outlook.com>
2024-03-24 12:37:23 +0100

    La til faggruppe teller

--- src/components/faggrupper/SearchArticles.tsx
+++ src/components/faggrupper/SearchArticles.tsx
@@ -36,23 +36,26 @@ export default function SearchArticles({ articles }: { articles: Article[] }) {
                 />
             </div>
 
+            <p className="px-4 pb-4">{filteredArticles.length} {filteredArticles.length == 1 ? (<>faggruppe</>) : (<>fagrupper</>)}</p>
+
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 pb-4">
                 {filteredArticles.map((article) => (
                     <Link key={article.title} href={article.href}>
-                        <div className="flex flex-col h-full p-4 border rounded-xl text-text-default border-gray-300 transition-all hover:-translate-y-1 hover:scale-105 hover:text-surface-action-selected-hover hover:border-border-action event-card">
+                        <div
+                            className="flex flex-col h-full p-4 border rounded-xl text-text-default border-gray-300 transition-all hover:-translate-y-1 hover:scale-105 hover:text-surface-action-selected-hover hover:border-border-action event-card">
                             <Heading level="2" size="small">{article.title}</Heading>
                             <div className="flex pt-2 flex-col gap-2 h-full">
                                 {article.when && (
                                     <Detail className="leading-normal">
                     <span className="flex items-center gap-1">
-                      <CalendarIcon title="person" /> {article.when}
+                      <CalendarIcon title="person"/> {article.when}
                     </span>
-                                </Detail>
+                                    </Detail>
                                 )}
                                 {article.audience && (
                                     <Detail className="leading-normal">
                                         <span className="flex items-center gap-1">
-                                          <PersonGroupIcon title="person" /> Målgruppe: {article.audience}
+                                          <PersonGroupIcon title="person"/> Målgruppe: {article.audience}
                                         </span>
                                     </Detail>
                                 )}

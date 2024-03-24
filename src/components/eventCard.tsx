3293b817ec6cc850a3379ce35d820f08dc3329fa
EIlif Johansen <eilifjohansen@outlook.com>
2024-03-24 14:28:28 +0100

    Fikset event tilbakeknapp

--- src/components/eventCard.tsx
+++ src/components/eventCard.tsx
@@ -49,7 +49,7 @@ export function EventCard({
                 <></>
             ) : (
                 <Link
-                    href={`/fagfestival/${event.event.id}`}
+                    href={`/event/${event.event.id}`}
                     key={`event-${event.event.id}`}
                     className="flex flex-col h-full p-4 border rounded-xl text-text-default border-gray-300 transition-all hover:-translate-y-1 hover:scale-105 hover:text-surface-action-selected-hover hover:border-border-action event-card"
                 >

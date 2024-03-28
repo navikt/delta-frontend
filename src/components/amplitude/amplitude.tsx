01652f6739073082a286a6a5590ef4a7515ecf2d
EIlif Johansen <eilifjohansen@outlook.com>
2024-03-28 21:41:55 +0100

    Nytt forsøk Amplitude bug

--- src/components/amplitude/amplitude.tsx
+++ src/components/amplitude/amplitude.tsx
@@ -1,11 +1,6 @@
 import { init } from "@amplitude/analytics-browser";
 
 export const initAmplitude = () => {
-    if ( typeof window !== "undefined" &&
-        window.location.hostname === "delta.nav.no") {
-        return;
-    }
-
     init("8cb400ce4d4338556a15507df2480a53", undefined, {
         serverUrl: "https://amplitude.nav.no/collect",
         serverZone: "EU",

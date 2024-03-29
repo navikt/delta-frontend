582093336db74a12fffb7926c95f362d9185e10c
EIlif Johansen <eilifjohansen@outlook.com>
2024-03-29 01:19:57 +0100

    Nytt forsøk amplitude fix med context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import "./globals.css";
+import AmplitudeContextProvider from "@/context/AmplitudeContext";
 
 type RootLayoutProps = {
     children: React.ReactNode;
@@ -14,7 +15,7 @@ export default async function RootLayout({ children }: RootLayoutProps) {
         <div id="main" className="flex flex-col min-h-screen" style={{background : "rgba(0, 80, 119, 1)"}}>
             <Header />
             <main className="bg-surface-subtle flex-grow flex justify-center pb-10">
-                {children}
+                <AmplitudeContextProvider>{children}</AmplitudeContextProvider>
             </main>
             <Footer />
             <ScrollToTop />

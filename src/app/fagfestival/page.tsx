a616cd8d6c4326ac0d3c06a74c1b5059fa981eb9
EIlif Johansen <eilifjohansen@outlook.com>
2024-04-09 12:12:59 +0200

    💬 Ny tekst på fafestival-siden

--- src/app/fagfestival/page.tsx
+++ src/app/fagfestival/page.tsx
@@ -1,5 +1,4 @@
 import { checkToken } from "@/auth/token";
-import CardWithBackground from "@/components/cardWithBackground";
 import CardWithBackgroundFagfestival from "@/components/fagfestival/cardWithBackground";
 import EventFilters from "@/components/fagfestival/eventFilters";
 import {getAllCategories, getEvents} from "@/service/eventActions";
@@ -20,13 +19,28 @@ export default async function Fagfestival() {
                 <div className="w-full">
                     <CardWithBackgroundFagfestival
                         color="bg-blue-200"
-                        title="Felles fagfestival 🎉"
+                        title="Fagfestival 2024"
                         backLink="/"
                     >
                         <div className="m-4 max-w-2xl font-serif">
-                            <h2 className="pb-4 text-2xl">Felles fagfestival for direktoratet 23.-25. april</h2>
-                            <p className="leading-normal">
-                                På denne siden finner du programmet og kan melde deg på aktiviteter.</p>
+                            <h2 className="pb-4 text-2xl">Hvordan navigere i programmet?</h2>
+                            <div className="prose">
+                                <p>Du får oversikt over hva som skjer på de ulike dagene ved å klikke på datoene
+                                    under.</p>
+                                <p>Dersom du er interessert i et foredrag kan du klikke deg inn for å få mer informasjon
+                                    om hva foredraget handler om og hvem som står på scenen.</p>
+                                <p>Når det gjelder workshops vil det være et begrenset antall plasser på disse. Her er
+                                    det «første-mann-til-mølla»-prinsippet som gjelder.</p>
+                                <p>Videre oppfordrer vi deg til å bruke «Bli med»-knappen som du finner øverst til høyre
+                                    hjørne på hvert arrangement. Da vil du automatisk få tilsendt en kalenderinvitasjon
+                                    til din innboks. I tillegg vil du enkelt kunne holde oversikten over dine
+                                    påmeldinger under «Mine påmeldinger» her på Delta.</p>
+                                <p>Du kan selv melde deg på så mange faglige innlegg du ønsker. På alt innhold (utenom
+                                    workshops) som foregår på fagfestivalen er det opp til hver enkelt å sikre seg en
+                                    stol i rommet. Dersom alle sitteplasser skulle være tatt vil det være mulig å
+                                    benytte seg av ståplasser, så fremst dette ikke er til hinder for selve
+                                    presentasjonen eller nødutganger.</p>
+                            </div>
                         </div>
                     </CardWithBackgroundFagfestival>
                 </div>
@@ -37,7 +51,7 @@ export default async function Fagfestival() {
                         title=""
                         newEvent
                     >
-                        <EventFilters categories={allCategories} selectCategory searchName homeTabs />
+                        <EventFilters categories={allCategories} selectCategory searchName homeTabs/>
                     </CardWithBackgroundFagfestival>
                 </div>
             </div>

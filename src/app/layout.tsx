c1c10a5d84a1ffb08ea9b5bedc1332e0e3c0407f
Ivan Skodje <ivanskodje@protonmail.com>
2024-04-24 15:21:01 +0200

    feat: lagt til en favicon for delta så vi ikke bruker default nextjs favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,34 @@ type RootLayoutProps = {
     children: React.ReactNode;
 };
 
+export const metadata: Metadata = {
+    icons: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        sizes: "16x16",
+        url: "/favicon/favicon.ico",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        url: "/favicon/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        url: "/favicon/favicon-16x16.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        url: "/favicon/apple-touch-icon.png",
+      },
+    ],
+  };
+
 export default async function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="no" className="min-h-screen">

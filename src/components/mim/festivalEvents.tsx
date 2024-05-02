403473843cb72c61b88dfe435a1bffe4b6debbb2
EIlif Johansen <eilifjohansen@outlook.com>
2024-05-02 12:53:12 +0200

    Fikset typo på mim siden

--- src/components/mim/festivalEvents.tsx
+++ src/components/mim/festivalEvents.tsx
@@ -10,7 +10,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 const fagfestivalCategory = "mim24";
 const activeDays = ["3", "8", "13", "15", "16"];
-const fagfestivalMonth = "Mai"
+const fagfestivalMonth = "mai"
 
 const getRemainingActiveDays = () => {
   const today = new Date();

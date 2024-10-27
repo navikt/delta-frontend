7bfc829db40b4e34affd06e3f62dcd7afffe2692
EIlif Johansen <eilifjohansen@outlook.com>
2024-10-28 00:32:32 +0100

    La til detaljer på administrer siden

--- src/app/event/[id]/admin/adminPage.tsx
+++ src/app/event/[id]/admin/adminPage.tsx
@@ -1,111 +1,129 @@
 "use client";
-import { FullDeltaEvent } from "@/types/event";
-import { BodyLong, Button, Heading, Modal, Search } from "@navikt/ds-react";
+import {FullDeltaEvent} from "@/types/event";
+import {BodyLong, Button, Heading, Modal, Search} from "@navikt/ds-react";
 import ParticipantTable from "./participantTable";
 import ExportParticipants from "../exportParticipants";
 import Link from "next/link";
-import { deleteEvent } from "@/service/eventActions";
-import { useEffect, useState } from "react";
-import { TrashIcon } from "@navikt/aksel-icons";
-import { User } from "@/types/user";
+import {deleteEvent} from "@/service/eventActions";
+import {useEffect, useState} from "react";
+import {CalendarIcon, ClockIcon, LocationPinIcon, TrashIcon} from "@navikt/aksel-icons";
+import {User} from "@/types/user";
+import {formatEventDates, formatEventDuration} from "@/service/format";
 
 type ParticipantPageProps = {
-  fullEvent: FullDeltaEvent;
-  user: User;
+    fullEvent: FullDeltaEvent;
+    user: User;
 };
 
 export default function AdminPage({
-  fullEvent: eventWithParticipants,
-  user,
-}: ParticipantPageProps) {
-  const [openConfirmation, setOpenConfirmation] = useState(false);
-  
-  const [searchInput, setSearchInput] = useState("");
-  const { event, participants, hosts }: FullDeltaEvent = eventWithParticipants;
-  
-  return (
-    <div className="flex flex-col gap-10">
+                                      fullEvent: eventWithParticipants,
+                                      user,
+                                  }: ParticipantPageProps) {
+    const [openConfirmation, setOpenConfirmation] = useState(false);
+
+    const [searchInput, setSearchInput] = useState("");
+    const {event, participants, hosts}: FullDeltaEvent = eventWithParticipants;
+
+    return (
+        <div className="flex flex-col gap-10">
       <span className="w-full flex flex-row justify-end">
         <span className="flex w-full md:w-fit flex-col md:flex-row gap-4">
           <Link
-            href={`/event/${event.id}/edit`}
-            className="w-full h-fit navds-button navds-button--primary whitespace-nowrap navds-label"
+              href={`/event/${event.id}/edit`}
+              className="w-full h-fit navds-button navds-button--primary whitespace-nowrap navds-label"
           >
             Rediger arrangement
           </Link>
           <Link
-            href={`/event/new?template=${event.id}`}
-            className="w-full h-fit navds-button navds-button--primary-neutral whitespace-nowrap navds-label"
+              href={`/event/new?template=${event.id}`}
+              className="w-full h-fit navds-button navds-button--primary-neutral whitespace-nowrap navds-label"
           >
             Bruk som mal
           </Link>
-          <ExportParticipants participants={participants} />
+          <ExportParticipants participants={participants}/>
           <Button
-            type="submit"
-            variant="danger"
-            className="w-full h-fit font-bold"
-            onClick={() => setOpenConfirmation((x) => !x)}
+              type="submit"
+              variant="danger"
+              className="w-full h-fit font-bold"
+              onClick={() => setOpenConfirmation((x) => !x)}
           >
             <span className="flex items-center gap-1">
-              <TrashIcon /> Slett
+              <TrashIcon/> Slett
             </span>
           </Button>
         </span>
       </span>
-        <Modal
-            open={openConfirmation}
-            onClose={() => setOpenConfirmation(false)}
-            aria-label="Slett arrangement modal"
-            className="w-4/5 max-w-[30rem] max-h-[50rem]"
-        >
-            <Modal.Body>
-                <Heading spacing level="1" size="large">
-                    {`Slett: "${event?.title}?"`}
-                </Heading>
-                <BodyLong spacing>
-                    {`Er du sikker på at du vil slette "${event?.title}"? Dette kan ikke angres.`}
-                </BodyLong>
-            </Modal.Body>
-            <Modal.Footer>
-                <Button
-                    variant="secondary"
-                    onClick={async () => setOpenConfirmation((x) => !x)}
-                >
-                    Avbryt
-                </Button>
-                <Button
-                    variant="danger"
-                    className="w-fit h-fit font-bold"
-                    onClick={() => deleteAndRedirect(event?.id!!)}
-                >
-                    Ja, jeg vil slette arrangementet
-                </Button>
-            </Modal.Footer>
-        </Modal>
-        <form>
-            <Search
-                className="pt-2 -mb-2 sm:w-64"
-                label="Søk alle deltakere"
-                variant="simple"
-                size="small"
-                value={searchInput}
-                onChange={(e) => {
-                    setSearchInput(e);
-                }}
+            <Modal
+                open={openConfirmation}
+                onClose={() => setOpenConfirmation(false)}
+                aria-label="Slett arrangement modal"
+                className="w-4/5 max-w-[30rem] max-h-[50rem]"
+            >
+                <Modal.Body>
+                    <Heading spacing level="1" size="large">
+                        {`Slett: "${event?.title}?"`}
+                    </Heading>
+                    <BodyLong spacing>
+                        {`Er du sikker på at du vil slette "${event?.title}"? Dette kan ikke angres.`}
+                    </BodyLong>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button
+                        variant="secondary"
+                        onClick={async () => setOpenConfirmation((x) => !x)}
+                    >
+                        Avbryt
+                    </Button>
+                    <Button
+                        variant="danger"
+                        className="w-fit h-fit font-bold"
+                        onClick={() => deleteAndRedirect(event?.id!!)}
+                    >
+                        Ja, jeg vil slette arrangementet
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <h2 className="navds-heading navds-heading--medium -mt-4">Detaljer</h2>
+            <span className="flex flex-row justify-start gap-2 items-center -mt-5">
+            <CalendarIcon aria-label="dato"/>
+                {formatEventDates(event)}
+            </span>
+            <span className="flex flex-row justify-start gap-2 items-center -mt-8">
+          <ClockIcon aria-label="tid"/>
+                {`${event.startTime.substring(11, 16)} – ${event.endTime.substring(
+                    11,
+                    16,
+                )}`}
+          </span>
+            <span className="flex flex-row justify-start gap-2 items-center -mt-8">
+            <LocationPinIcon aria-label="sted"/>
+                {event.location}
+          </span>
+            <h2 className="navds-heading navds-heading--medium mt-1">Deltakerliste</h2>
+            <form>
+                <Search
+                    className="pt-0 -mt-4 -mb-2 sm:w-64"
+                    label="Søk alle deltakere"
+                    variant="simple"
+                    size="small"
+                    value={searchInput}
+                    onChange={(e) => {
+                        setSearchInput(e);
+                    }}
+                />
+            </form>
+            <ParticipantTable
+                participants={participants}
+                hosts={hosts}
+                event={event}
+                user={user}
+                searchInput={searchInput}
             />
-        </form>
-      <ParticipantTable
-        participants={participants}
-        hosts={hosts}
-        event={event}
-        user={user}
-        searchInput={searchInput}
-      />
-    </div>
-  );
+        </div>
+    );
 }
 
 async function deleteAndRedirect(eventId: string) {
-  await deleteEvent(eventId);
-  window.location.href = "/";
+    await deleteEvent(eventId);
+    window.location.href = "/";
 }

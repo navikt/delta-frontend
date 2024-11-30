730d37f69d379c0fffa9340e908ad9511fa54d53
EIlif Johansen <eilifjohansen@outlook.com>
2024-11-30 18:50:55 +0100

    Implementerer import Head from "next/head";

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,27 @@ import { checkToken } from "@/auth/token";
 import CardWithBackground from "@/components/cardWithBackground";
 import EventFilters from "@/components/eventFilters";
 import { Metadata } from "next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Delta Δ Nav",
   description: "Påmeldingsapp",
 };
+
 export default async function Home(context: any) {
   await checkToken("/");
 
   return (
-    <CardWithBackground
-      title="Arrangementer"
-      newEvent
-    >
-      <EventFilters selectCategory searchName homeTabs ctaLink joinedLink selectTimeRadio />
-    </CardWithBackground>
+      <>
+        <Head>
+          <title>Delta Δ Nav</title>
+        </Head>
+        <CardWithBackground
+            title="Arrangementer"
+            newEvent
+        >
+          <EventFilters selectCategory searchName homeTabs ctaLink joinedLink selectTimeRadio />
+        </CardWithBackground>
+      </>
   );
 }

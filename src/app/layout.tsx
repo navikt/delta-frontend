1bf5791973becf94bce7fe9ed5b4fd0f1f598bdd
EIlif Johansen <eilifjohansen@outlook.com>
2024-11-30 17:08:15 +0100

    Tester UmamiContextProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,10 @@ import Header from "@/components/header";
 import Footer from "@/components/footer";
 import ScrollToTop from "@/components/ScrollToTop";
 import "./globals.css";
-import AmplitudeContextProvider from "@/context/AmplitudeContext";
+/* import AmplitudeContextProvider from "@/context/AmplitudeContext"; */
+import { UmamiContextProvider } from "@/context/UmamiContextProvider";
 import { Metadata } from "next";
-import {UmamiTracker} from "@/components/umami/umami";
+/* import {UmamiTracker} from "@/components/umami/umami"; */
 
 type RootLayoutProps = {
     children: React.ReactNode;
@@ -42,11 +43,10 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     return (
         <html lang="no" className="min-h-screen">
         <body>
-        <UmamiTracker />
         <div id="main" className="flex flex-col min-h-screen" style={{background: "rgba(19,17,54)"}}>
             <Header/>
             <main className="bg-surface-subtle flex-grow flex justify-center">
-            <AmplitudeContextProvider>{children}</AmplitudeContextProvider>
+            <UmamiContextProvider>{children}</UmamiContextProvider>
             </main>
             <Footer />
             <ScrollToTop />

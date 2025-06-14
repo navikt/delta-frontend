984237c483da9bbbc4f08cbf9ee7e56db28acff8
EIlif Johansen <eilifjohansen@outlook.com>
2025-06-14 15:14:42 +0200

    Tester Ping

--- src/context/UmamiContextProvider.tsx
+++ src/context/UmamiContextProvider.tsx
@@ -14,19 +14,31 @@ interface UmamiProviderProps {
 
 export const UmamiContextProvider: React.FC<UmamiProviderProps> = ({ children }) => {
     useEffect(() => {
-        // Dynamically create and append the script
-        const script = document.createElement("script");
-        script.src = "https://cdn.nav.no/team-researchops/sporing/sporing.js";
-        script.setAttribute("data-domains", "delta.nav.no");
-        script.setAttribute("data-host-url", "https://umami.nav.no");
-        script.setAttribute("data-website-id", "efe951d8-ebbb-4fad-938e-91eee190f6aa");
-        script.defer = true;
+        // Dynamically create and append the first script
+        const umamiScript = document.createElement("script");
+        umamiScript.src = "https://cdn.nav.no/team-researchops/sporing/sporing.js";
+        umamiScript.setAttribute("data-domains", "delta.nav.no");
+        umamiScript.setAttribute("data-host-url", "https://umami.nav.no");
+        umamiScript.setAttribute("data-website-id", "efe951d8-ebbb-4fad-938e-91eee190f6aa");
+        umamiScript.defer = true;
 
-        document.body.appendChild(script);
+        // Dynamically create and append the second script
+        const pingScript = document.createElement("script");
+        pingScript.src = "https://cdn.nav.no/team-researchops/ping/v1/ping.js";
+        pingScript.setAttribute("data-ping-endpoint", "https://ping.nav.no/event");
+        pingScript.setAttribute("data-ping-team", "delta");
+        pingScript.setAttribute("data-ping-app", "delta-frontend");
+        pingScript.setAttribute("data-ping-environment", "prod");
+        pingScript.setAttribute("data-ping-track-pageviews", "true");
+        pingScript.defer = true;
+
+        document.body.appendChild(umamiScript);
+        document.body.appendChild(pingScript);
 
         return () => {
-            // Cleanup the script when the component unmounts
-            document.body.removeChild(script);
+            // Cleanup the scripts when the component unmounts
+            document.body.removeChild(umamiScript);
+            document.body.removeChild(pingScript);
         };
     }, []);
 
@@ -36,3 +48,4 @@ export const UmamiContextProvider: React.FC<UmamiProviderProps> = ({ children })
         </UmamiContext.Provider>
     );
 };
+

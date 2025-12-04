ce54264e480ab1bdf0f08e50ec75aad543c00f33
EIlif Johansen <eilifjohansen@outlook.com>
2025-12-05 00:42:09 +0100

    Endret fra spinner til progressbar

--- src/app/statistikk/loading.tsx
+++ src/app/statistikk/loading.tsx
@@ -1,12 +1,16 @@
 import CardWithBackground from "@/components/cardWithBackground";
-import { Loader } from "@navikt/ds-react";
+import { ProgressBar } from "@navikt/ds-react";
 
 export default function Loading() {
   return (
     <CardWithBackground title="Delta">
       <div className="flex flex-col items-center justify-center py-20 space-y-4">
-        <Loader size="3xlarge" title="Laster statistikk..." />
-        <p className="text-lg text-gray-600">Laster statistikk, dette tar ca. 10 sekunder...</p>
+        <ProgressBar
+          size="large"
+          simulated={{ seconds: 10, onTimeout: () => {} }}
+          aria-label="Laster statistikk"
+        />
+        <p className="text-lg">Laster statistikk, dette tar ca. 10 sekunder...</p>
       </div>
     </CardWithBackground>
   );

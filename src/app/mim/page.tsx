0d55500f9feee5646a700662d1e11a781244103b
EIlif Johansen <eilifjohansen@outlook.com>
2026-04-21 15:05:09 +0200

    Sender param til url

--- src/app/mim/page.tsx
+++ src/app/mim/page.tsx
@@ -3,7 +3,6 @@ import CardWithBackground from "@/components/cardWithBackground";
 import "./fagfestival.css";
 
 import { Metadata } from "next";
-import { Heading } from "@navikt/ds-react";
 import FagfestivalEvents from "@/components/fagfestival/fagfestivalEvents";
 
 export const metadata: Metadata = {
@@ -18,11 +17,14 @@ export default async function Fagfestival() {
     <>
       <div className="flex flex-col w-full colorful pb-10 -mb-4">
         <div className="w-full relative z-20">
-          <div className="mt-8 mb-6 px-4 ax-sm:px-12 max-w-[80rem] mx-auto w-full">
-            <Heading level="1" size="xlarge" style={{ color: "#021841" }}>
-              Mangfold i mai
-            </Heading>
-          </div>
+          <CardWithBackground className="bg-fagfestival" newEvent>
+            <CardWithBackground
+              compactHeader
+              titleNode={"Mangfold i mai"}
+              titleColor={"#021841"}
+            >
+            </CardWithBackground>
+          </CardWithBackground>
         </div>
 
         <div className="w-full -mt-28 relative z-10">

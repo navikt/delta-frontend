b2703972635e51699b12fd337102ced0feb5627a
EIlif Johansen <eilifjohansen@outlook.com>
2026-04-22 16:08:19 +0200

    Tok bort ubrukt kode

--- src/app/mim-archive/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default async function NotFound() {
-  return (
-    <main className="flex flex-grow">
-      <section className="w-screen flex-grow flex justify-center items-center">
-        404 - Siden finnes ikke
-      </section>
-    </main>
-  );
-}

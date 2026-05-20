147059d6df8201dd183d3d1ec7f509ad0a00cf50
EIlif Johansen <eilifjohansen@outlook.com>
2026-05-21 00:27:56 +0200

    la til ips festivalside

---
+++ src/app/ips/page.tsx
@@ -0,0 +1,37 @@
+import { checkToken } from "@/auth/token";
+import CardWithBackground from "@/components/cardWithBackground";
+import "./fagfestival.css";
+
+import { Metadata } from "next";
+import FagfestivalEvents from "@/components/fagfestival/fagfestivalEvents";
+
+export const metadata: Metadata = {
+  title: "IPS Δ Delta",
+  description: "Påmeldingsapp",
+};
+
+export default async function Fagfestival() {
+  await checkToken("/ips");
+
+  return (
+    <>
+      <div className="flex flex-col w-full colorful pb-10 -mb-4">
+        <div className="w-full relative z-20">
+          <CardWithBackground
+            title="IPS-konferansen 2026: Open space workshop"
+            titleColor="#021841"
+            className="bg-fagfestival"
+            newEvent
+          >
+            <FagfestivalEvents
+              category="ips"
+              activeDays={["4"]}
+              month="juni"
+              slug="ips"
+            />
+          </CardWithBackground>
+        </div>
+      </div>
+    </>
+  );
+}
